1637506e8f1e268885e305f807538c05afe3eb43
Suman Gole <gole35830@gmail.com>
2022-12-03 18:23:30 +0545

    add: variables in python

---
+++ Basics/variables.py
@@ -0,0 +1,14 @@
+name = "suman" # string
+age = 20 # integer
+# date_of_birth = '2002-01-08'
+address = "Lalitpur, Nepal" # string
+percentage = 99.8  #float
+
+
+if __name__ == '__main__':
+    print(f"My name is {name} and I am {age} years old. I live in {address}")
+    print(type(name))
+    print(type(age))
+    print(type(address))
+    print(type(percentage))
+
