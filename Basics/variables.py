443f1350ec8d03aa298f4dfd852e22bdb9536f75
Suman Gole <gole35830@gmail.com>
2022-12-03 18:25:27 +0545

    add: basic variables in python

--- Basics/variables.py
+++ Basics/variables.py
@@ -7,7 +7,7 @@ percentage = 99.8  #float
 
 if __name__ == '__main__':
     print(f"My name is {name} and I am {age} years old. I live in {address}")
-    print(type(name))
+    print(type(name)) 
     print(type(age))
     print(type(address))
     print(type(percentage))
