97c46bcad7f8b4ca51a7164c93dfab6280817fb7
Suman Gole <gole35830@gmail.com>
2022-12-10 13:13:12 +0545

    add method overriding in python

---
+++ OOPS/method_overring.py
@@ -0,0 +1,31 @@
+class Vehicle(object):
+
+    Tax = 0.13
+
+    def __init__(self, name, vehicle_type) -> None:
+        self.name = name
+        self.vehicle_type = vehicle_type
+
+    def get_details(self):
+        return f"{self.name} {self.vehicle_type}"
+
+    def update_tax(cls, new_tax):
+        cls.Tax = new_tax
+
+
+class Car(Vehicle):
+    def __init__(self, name, vehicle_type, brand) -> None:
+        super().__init__(name, vehicle_type)
+        self.brand = brand
+
+    def get_details(self):
+        return super().get_details() + " " + self.brand
+
+
+if __name__ == "__main__":
+
+    c1 = Car("Avalon", "Car", "Toyota")
+    details = c1.get_details()
+    c1.update_tax(0.14)
+    print(c1.Tax)
+    print(details)
